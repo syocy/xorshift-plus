3851216e8fc875b9d080588e787d975ef8c4068f
OSANAI Kazuyoshi <osmium.k@gmail.com>
2019-01-26 16:52:45 +0900

    remove warning

--- benchmarks/micro/micro.hs
+++ benchmarks/micro/micro.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
 import Gauge.Main
@@ -13,36 +15,36 @@ import System.CPUTime
 size :: Int
 size = 1000
 
-getSeed :: (Num a) => IO a
+getSeed :: Num a => IO a
 getSeed = fromInteger `fmap` getCPUTime
 
 getRandomIntList :: Int -> IO [Int]
 getRandomIntList n = do
-  seed <- getSeed
+  seed <- getSeed @ Int
   xorshift <- genXorshiftPlusInt seed
   sequence $ replicate n $ getInt xorshift
 
 getRandomWordList :: Int -> IO [Word]
 getRandomWordList n = do
-  seed <- getSeed
+  seed <- getSeed @ Word
   xorshift <- genXorshiftPlusWord seed
   sequence $ replicate n $ getWord xorshift
 
 getRandomIntList_xorshift_Int32 :: Int -> IO [Int32]
 getRandomIntList_xorshift_Int32 n = do
-  seed <- getSeed
+  seed <- getSeed @ Int32
   let xorshift = makeXorshift32 seed
   return $ take n $ randoms xorshift
 
 getRandomIntList_xorshift_Int64 :: Int -> IO [Int64]
 getRandomIntList_xorshift_Int64 n = do
-  seed <- getSeed
+  seed <- getSeed @ Int64
   let xorshift = makeXorshift64 seed
   return $ take n $ randoms xorshift
 
 getRandomWord64List_Xorshift128Plus :: Int -> IO [Word64]
 getRandomWord64List_Xorshift128Plus n = do
-  seed <- getSeed
+  seed <- getSeed @ Word64
   let xorshift = X128.initialize seed
   return $ take n $ makeList xorshift
   where
