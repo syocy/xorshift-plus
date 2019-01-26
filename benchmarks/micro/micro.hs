6293f2a2c09806f20fafe4292ecef3d6aeb4b53c
OSANAI Kazuyoshi <osmium.k@gmail.com>
2019-01-26 16:12:34 +0900

    benchmark seed from timestamp

--- benchmarks/micro/micro.hs
+++ benchmarks/micro/micro.hs
@@ -8,50 +8,53 @@ import Random.XorshiftPlus
 import Random.Xorshift.Int32
 import Random.Xorshift.Int64
 import qualified System.Random.Xorshift128Plus as X128
+import System.CPUTime
 
 size :: Int
 size = 1000
 
-getRandomIntList :: Int -> Int -> IO [Int]
-getRandomIntList n seed = do
+getSeed :: (Num a) => IO a
+getSeed = fromInteger `fmap` getCPUTime
+
+getRandomIntList :: Int -> IO [Int]
+getRandomIntList n = do
+  seed <- getSeed
   xorshift <- genXorshiftPlusInt seed
   sequence $ replicate n $ getInt xorshift
 
-getRandomWordList :: Int -> Word -> IO [Word]
-getRandomWordList n seed = do
+getRandomWordList :: Int -> IO [Word]
+getRandomWordList n = do
+  seed <- getSeed
   xorshift <- genXorshiftPlusWord seed
   sequence $ replicate n $ getWord xorshift
 
-getRandomIntList_xorshift_Int32 :: Int -> Int -> [Int32]
-getRandomIntList_xorshift_Int32 n seed =
-  let xorshift = makeXorshift32 seed in
-  take n $ randoms xorshift
+getRandomIntList_xorshift_Int32 :: Int -> IO [Int32]
+getRandomIntList_xorshift_Int32 n = do
+  seed <- getSeed
+  let xorshift = makeXorshift32 seed
+  return $ take n $ randoms xorshift
 
-getRandomIntList_xorshift_Int64 :: Int -> Int -> [Int64]
-getRandomIntList_xorshift_Int64 n seed =
-  let xorshift = makeXorshift64 seed in
-  take n $ randoms xorshift
+getRandomIntList_xorshift_Int64 :: Int -> IO [Int64]
+getRandomIntList_xorshift_Int64 n = do
+  seed <- getSeed
+  let xorshift = makeXorshift64 seed
+  return $ take n $ randoms xorshift
 
-getRandomWord64List_Xorshift128Plus :: Int -> Word64 -> [Word64]
-getRandomWord64List_Xorshift128Plus n seed =
-  let xorshift = X128.initialize seed in
-  take n $ makeList xorshift
+getRandomWord64List_Xorshift128Plus :: Int -> IO [Word64]
+getRandomWord64List_Xorshift128Plus n = do
+  seed <- getSeed
+  let xorshift = X128.initialize seed
+  return $ take n $ makeList xorshift
   where
     makeList gen = let (x, gen') = X128.next gen in x : (makeList gen')
 
 main :: IO ()
-main = defaultMain [
-    bgroup "variableSeeds"
-      [ bench "seed_1" $ nfAppIO (getRandomIntList size) 1
-      , bench "seed_-1" $ nfAppIO (getRandomIntList size) (-1)
-      , bench "seed_INT_MIN" $ nfAppIO (getRandomIntList size) (minBound :: Int)
-      , bench "seed_INT_MAX" $ nfAppIO (getRandomIntList size) (maxBound :: Int)
-      ],
-    bgroup "compareWithOtherPRNGs"
-      [ bench "xorshift-plus_Int (THIS PACKAGE)" $ nfAppIO (getRandomIntList size) 1
-      , bench "xorshift-plus_Word (THIS PACKAGE)" $ nfAppIO (getRandomWordList size) 1
-      , bench "xorshift_Int32" $ nf (getRandomIntList_xorshift_Int32 size) 1
-      , bench "xorshift_Int64" $ nf (getRandomIntList_xorshift_Int64 size) 1
-      , bench "Xorshift128Plus_Word64" $ nf (getRandomWord64List_Xorshift128Plus size) 1
-      ]
+main = defaultMain
+  [ bgroup "compareWithOtherPRNGs"
+    [ bench "xorshift-plus_Int (THIS PACKAGE)" $ nfIO (getRandomIntList size)
+    , bench "xorshift-plus_Word (THIS PACKAGE)" $ nfIO (getRandomWordList size)
+    , bench "xorshift_Int32" $ nfIO (getRandomIntList_xorshift_Int32 size)
+    , bench "xorshift_Ina64" $ nfIO (getRandomIntList_xorshift_Int64 size)
+    , bench "Xorshift128Plus_Word64" $ nfIO (getRandomWord64List_Xorshift128Plus size)
+    ]
   ]
